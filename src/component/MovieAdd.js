371c33ffdd6b8b53fd95a99cfbaad72b59bd5096
unknown <mohamedali.bouzaiene@esprit.tn>
2020-08-09 18:16:11 +0100

    fixed everything

--- src/component/MovieAdd.js
+++ src/component/MovieAdd.js
@@ -1,56 +1,74 @@
-import React, { useState, setState } from 'react'
-import { Modal, Button } from 'react-bootstrap'
-import Rater from './MoviefilterRate'
+import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
+import Rater from "./MoviefilterRate";
 
 function Movieadd(props) {
-    const [show, setShow] = useState(false);
-    const newmovie = {
-        title: "",
-        description: "",
-        posterUrl: "",
-        rating: ""
-    }
-    const [nemovie, setNemovie] = useState(newmovie)
+  const [show, setShow] = useState(false);
+  const [newMovie, setNewMovie] = useState({
+    title: "",
+    description: "",
+    posterUrl: "",
+    rating: "",
+  });
 
-    function getrate(str){
-        nemovie.rating=str;
-    }
+  function getrate(str) {
+    newMovie.rating = str;
+  }
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    
-    
-    return (
-        <>
-            <Button variant="primary" onClick={handleShow}>
-                Add a movie
-        </Button>
+  function handleChange(e) {
+    setNewMovie({ ...newMovie, [e.target.name]: e.target.value });
+  }
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-            <Modal show={show} onHide={handleClose} animation={false}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Write your movie's info</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <input type='text' placeholder='The title' onChange={(e) => { nemovie.title = e.target.value }}></input>
-                    <input type='text' placeholder='Description' onChange={(e) => { nemovie.description = e.target.value }}></input>
-                    <input type='text' placeholder='Poster Url' onChange={(e) => { nemovie.posterUrl = e.target.value }}></input>
-                    <Rater handle={getrate}/>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-            </Button>
-                    <Button variant="primary" onClick={
-                        () => {
-                        setShow(false);
-                        props.handleChange(nemovie)
-                    }}>
-                        Add
-            </Button>
-                </Modal.Footer>
-            </Modal>
-        </>
-    );
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Add a movie
+      </Button>
+
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Write your movie's info</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <input
+            type="text"
+            name="title"
+            placeholder="The title"
+            onChange={handleChange}
+          ></input>
+          <input
+            type="text"
+            name="description"
+            placeholder="Description"
+            onChange={handleChange}
+          ></input>
+          <input
+            type="text"
+            name="posterUrl"
+            placeholder="Poster Url"
+            onChange={handleChange}
+          ></input>
+          <Rater handle={getrate} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => {
+              setShow(false);
+              props.handleChange(newMovie);
+            }}
+          >
+            Add
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
 
 export default Movieadd;
